cde899496f7c03be078a8a028657fc3388fcf573
Ricardo MR <ricardo.martinez@bec.seguridad.unam.mx>
2020-02-15 08:24:38 -0600

    Homework 4 done...

--- Homework4/ConsoleExplorer.cs
+++ Homework4/ConsoleExplorer.cs
@@ -1,10 +1,219 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.IO;
+using System.Linq;
 
 namespace Homework4
 {
     class ConsoleExplorer
     {
+        public string CurrentPath { get; private set; }
+
+        public ConsoleExplorer()
+        {
+            this.CurrentPath = Environment
+                .GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        }
+
+        /// <summary>
+        /// Lists contents of directory in CurrentPath property
+        /// </summary>
+        /// <returns>Formatted string with files and directories.</returns>
+        public string ListContents()
+        {
+            // Call method with current path
+            return this.ListContents(this.CurrentPath);
+        }
+
+        /// <summary>
+        /// Lists contents of specified directory.
+        /// </summary>
+        /// <param name="directoryPath">Directory path</param>
+        /// <returns>Formatted string with files and directories.</returns>
+        public string ListContents(string directoryPath)
+        {
+            try
+            {
+                // Full path 
+                string auxPath = Path.GetFullPath(directoryPath,
+                                                  this.CurrentPath);
+                // All files
+                string files = String.Join('\n', Directory
+                    .GetFiles(auxPath).Select(Path.GetFileName));
+                // All directories
+                string directories = String.Join('\n', Directory
+                    .GetDirectories(auxPath).Select(Path.GetFileName));
+                // Formatting
+                return $"FILES:\n{files}\n\nDIRECTORIES:\n{directories}";
+            }
+            // Access denied
+            catch (UnauthorizedAccessException)
+            {
+                return "Access denied...";
+            }
+            // Path too long (more than 260 characters)
+            catch (PathTooLongException)
+            {
+                return "Path is too long...";
+            }
+            // Directory does not exist
+            catch (DirectoryNotFoundException)
+            {
+                return "Directory not found...";
+            }
+            // Others
+            catch (IOException)
+            {
+                return "IOException...";
+            }
+        }
+
+        /// <summary>
+        /// Change CurrentPath property
+        /// </summary>
+        /// <param name="directoryPath">New current path</param>
+        /// <returns>Error messages or null if successfull.</returns>
+        public string ChangeDirectory(string directoryPath)
+        {
+            try
+            {
+                // Move 'up'
+                if (directoryPath.Equals(".."))
+                {
+                    this.CurrentPath = Directory.GetParent(this.CurrentPath)
+                        .FullName;
+                }
+                // Validate new path
+                else
+                {
+                    // New path
+                    string newPath = Path.GetFullPath(directoryPath,
+                                                      this.CurrentPath);
+                    // Does not exist...
+                    if (!Directory.Exists(newPath))
+                    {
+                        throw new DirectoryNotFoundException();
+                    }
+                    // Change current path
+                    this.CurrentPath = newPath;
+                }
+                // Success!
+                return null;
+            }
+            // No Access
+            catch (UnauthorizedAccessException)
+            {
+                return "Access denied...";
+            }
+            // Path too long (more than 260 characters)
+            catch (PathTooLongException)
+            {
+                return "Path is too long...";
+            }
+            // Directory does not exist
+            catch (DirectoryNotFoundException)
+            {
+                return "Directory not found...";
+            }
+            // Parent of root?!
+            catch (NullReferenceException)
+            {
+                return "Root has no parent directory...";
+            }
+            // Others
+            catch (IOException)
+            {
+                return "IOException...";
+            }
+        }
+
+        /// <summary>
+        /// Create file specified by path
+        /// </summary>
+        /// <param name="filePath">File path</param>
+        /// <returns>Error messages or null if successfull.</returns>
+        public string CreateFile(string filePath)
+        {
+            // Full path
+            string auxPath = Path.GetFullPath(filePath, this.CurrentPath);
+            // File exists...
+            if (File.Exists(auxPath))
+            {
+                return "The file already exists...";
+            }
+            try
+            {
+                // File creation
+                File.Create(auxPath);
+                // Success!
+                return null;
+            }
+            // Access denied
+            catch (UnauthorizedAccessException)
+            {
+                return "Access denied...";
+            }
+            // Path too long (more than 260 characters)
+            catch (PathTooLongException)
+            {
+                return "Path is too long...";
+            }
+            // Directory is not valid
+            catch (DirectoryNotFoundException)
+            {
+                return "Directory not found...";
+            }
+            // Others
+            catch (IOException)
+            {
+                return "IOException...";
+            }
+        }
+
+        /// <summary>
+        /// Move/Rename file to another location
+        /// </summary>
+        /// <param name="filePath">Original file</param>
+        /// <param name="directoryPath">New location/name</param>
+        /// <returns>Error messages or null if successfull.</returns>
+        public string MoveFile(string filePath, string directoryPath)
+        {
+            // Origin path
+            string path1 = Path.GetFullPath(filePath, this.CurrentPath);
+            // Destination path
+            string path2 = Path.GetFullPath(directoryPath, this.CurrentPath);
+            // File does not exist...
+            if (!File.Exists(path1))
+            {
+                return "File not found...";
+            }
+            try
+            {
+                // Move file
+                File.Move(path1, path2, true);
+                // Success!
+                return null;
+            }
+            // No access
+            catch (UnauthorizedAccessException)
+            {
+                return "Access denied...";
+            }
+            // Path too long (more than 260 characters)
+            catch (PathTooLongException)
+            {
+                return "Path is too long...";
+            }
+            // Invalid directory
+            catch (DirectoryNotFoundException)
+            {
+                return "Directory not found...";
+            }
+            // Others
+            catch (IOException)
+            {
+                return "IOException...";
+            }
+        }
+
     }
 }
--- Homework4/Program.cs
+++ Homework4/Program.cs
@@ -1,12 +1,173 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Homework4
 {
     class Program
     {
+        /// <summary>
+        /// Console Simulation...
+        /// </summary>
+        /// <param name="args">Not needed</param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Console
+            ConsoleExplorer explorer = new ConsoleExplorer();
+            // Command history
+            List<string> history = new List<string>();
+            // User input
+            string input;
+            // Method result
+            string result;
+            // String.Split auxiliar
+            string[] arguments;
+            do
+            {
+                // Console prompt
+                Console.Write($"{explorer.CurrentPath}> ");
+                // Input
+                input = Console.ReadLine();
+                // Split input (all spacing)
+                arguments = Regex.Split(input, @"\s+")
+                    .Where(s => !String.IsNullOrEmpty(s)).ToArray();
+                // No command, continue...
+                if (arguments.Length == 0)
+                {
+                    continue;
+                }
+                // Add command to history
+                history.Add(input);
+                // Commands
+                switch (arguments[0])
+                {
+                    // dir command
+                    case "dir":
+                        // No arguments, use current path
+                        if (arguments.Length == 1)
+                        {
+                            result = explorer.ListContents();
+                        }
+                        // 1 argument, use specified path
+                        else if (arguments.Length == 2)
+                        {
+                            result = explorer.ListContents(arguments[1]);
+                        }
+                        // No need for more than 1 argument, fail...
+                        else
+                        {
+                            result = "Too many arguments...\n";
+                        }
+                        // Print contents
+                        Console.WriteLine(result);
+                        break;
+                    // cd command
+                    case "cd":
+                        // 1 argument, try to change path
+                        if (arguments.Length == 2)
+                        {
+                            result = explorer.ChangeDirectory(arguments[1]);
+                        }
+                        // No arguments, fail...
+                        else if (arguments.Length == 1)
+                        {
+                            result = "Directory path is missing...";
+                        }
+                        // No need for more than 1 argument, fail...
+                        else
+                        {
+                            result = "Too many arguments...";
+                        }
+                        // result is not null -> error...
+                        if (result != null)
+                        {
+                            Console.WriteLine(result);
+                        }
+                        break;
+                    // touch command
+                    case "touch":
+                        // 1 argument, try to create file
+                        if (arguments.Length == 2)
+                        {
+                            result = explorer.CreateFile(arguments[1]);
+                        }
+                        // No file path/name, fail...
+                        else if (arguments.Length == 1)
+                        {
+                            result = "File path is missing...";
+                        }
+                        // No need for more than 1 argument, fail...
+                        else
+                        {
+                            result = "Too many arguments...";
+                        }
+                        // result is not null -> error...
+                        if (result != null)
+                        {
+                            Console.WriteLine(result);
+                        }
+                        break;
+                    // move command
+                    case "move":
+                        // 2 arguments, try to move file
+                        if (arguments.Length == 3)
+                        {
+                            result = explorer.MoveFile(arguments[1],
+                                                       arguments[2]);
+                        }
+                        // 1 or 2 paths missing, fail...
+                        else if (arguments.Length < 3)
+                        {
+                            result = "File/Destination path is missing...";
+                        }
+                        // No need for more than 2 arguments, fail...
+                        else
+                        {
+                            result = "Too many arguments...";
+                        }
+                        // result is not null -> error...
+                        if (result != null)
+                        {
+                            Console.WriteLine(result);
+                        }
+                        break;
+                    // history command
+                    case "history":
+                        // 1 or more arguments, fail...
+                        if (arguments.Length != 1)
+                        {
+                            Console.WriteLine("No arguments...");
+                            continue;
+                        }
+                        // Print used commands
+                        foreach (string cmd in history)
+                        {
+                            Console.WriteLine(cmd);
+                        }
+                        break;
+                    // cls command
+                    case "cls":
+                        // 1 or more rguments, fail...
+                        if (arguments.Length != 1)
+                        {
+                            Console.WriteLine("No arguments...");
+                            continue;
+                        }
+                        // Clear console
+                        Console.Clear();
+                        break;
+                    // exit command
+                    case "exit":
+                        // Nothing
+                        break;
+                    default:
+                        // Unknown command, fail...
+                        Console.WriteLine("Unknown command...");
+                        break;
+                }
+                // Waiting for exit command
+            } while (!input.Equals("exit"));
         }
     }
 }
